2ff9ff91a5cb79b18b73ba9f36e25859f34af46f
Saritha <varshith.kola@gmail.com>
2021-03-29 09:47:32 -0700

    modified:   src/main/java/com/example/Calculator/Interfaces/AdvancedOperations.java 	modified:   target/classes/com/example/Calculator/Interfaces/AdvancedOperations.class

--- src/main/java/com/example/Calculator/Interfaces/AdvancedOperations.java
+++ src/main/java/com/example/Calculator/Interfaces/AdvancedOperations.java
@@ -1,5 +1,14 @@
 package com.example.Calculator.Interfaces;
 
 public interface AdvancedOperations {
+	
+	public double square(double number);
+	public double cube(double x);
+	public double power(double base, double exponent);
+	public double absolute(double x);
+	public double remainder(double dividend, double divisor);
+	
+	
+	
 
 }
