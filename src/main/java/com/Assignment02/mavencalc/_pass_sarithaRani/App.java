3a4140d996f6330439d330e4080c50e11656f971
Saritha <varshith.kola@gmail.com>
2021-03-29 08:58:20 -0700

    created project

---
+++ src/main/java/com/Assignment02/mavencalc/_pass_sarithaRani/App.java
@@ -0,0 +1,13 @@
+package com.Assignment02.mavencalc._pass_sarithaRani;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/Assignment02/mavencalc/_pass_sarithaRani/AppTest.java
@@ -0,0 +1,20 @@
+package com.Assignment02.mavencalc._pass_sarithaRani;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
